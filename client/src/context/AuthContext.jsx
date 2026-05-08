bd9dad7d5dd20207e153717305fad917af0bcbf4
Lr-491 <lrafiou491@gmail.com>
2026-05-08 05:06:21 +0100

    configuration du fichier AuthContext

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,58 @@
+AuthContext/**
+ * Context d'authentification
+ * Gère l'utilisateur connecté dans toute l'application
+ */
+
+import { createContext, useContext, useState, useEffect } from "react";
+import { authAPI } from "../api/api";
+
+
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [ user,setUser ] = useState(null)
+    const [ loading, setLoading ] = useState(true);
+
+    // Vérifier si un token existeau chargement de l'app
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if(token) {
+            authAPI.me()
+                .then(data => setUser(data.user))
+                .catch(() => localStorage.removeItem('token'))
+                .finally(() => setLoading(false));
+        } else {
+            setLoading(false)
+        }
+    },[]);
+
+    /**
+     * Connexion -> stocke le token et l'utlisateur 
+     */
+    const login = (token, userData) => {
+        localStorage.setItem('token', token);
+        setUser(userData)
+    };
+
+    /**
+     * Déconnexion -> Supprime le token et l'utilisateur 
+     */
+    const logout = () => {
+        localStorage.removeItem('token');
+        setUser(null)
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, loading, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+
+/**
+ * Hook pouutiliser le contexte d'auth
+ * Usage : const{ user,login, logout } = useAuth();
+ */
+export const useAuth = () => useContext(AuthContext);
